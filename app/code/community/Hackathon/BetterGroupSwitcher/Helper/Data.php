1cbc2c2342ef72b90313db5fca5719cf4df6c783
Fabian Blechschmidt <digicomp-atlassian-net@fabian-blechschmidt.de>
2014-05-10 16:14:23 +0200

    started module

---
+++ app/code/community/Hackathon/BetterGroupSwitcher/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fabian
+ * Date: 10.05.14
+ * Time: 16:10
+ */ 
+class Hackathon_BetterGroupSwitcher_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/community/Hackathon/BetterGroupSwitcher/sql/hackathon_bettergroupswitcher_setup/install-1.0.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fabian
+ * Date: 10.05.14
+ * Time: 16:10
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
